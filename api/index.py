ee6b233be2b713cebe1af3c89d839bec6f81d15b
Matheus Rodrigues de Lima <mhs.djc@gmail.com>
2026-02-04 20:14:10 -0300

    remove endpoint de geração de código de barras: elimina rota e simplifica a estrutura do arquivo index.py

--- api/index.py
+++ api/index.py
@@ -1,7 +1,5 @@
-from flask import Flask, request, send_file, abort
-import barcode
-from barcode.writer import ImageWriter
-import io
+from flask import Flask
+
 
 app = Flask(__name__)
 
@@ -9,16 +7,3 @@ app = Flask(__name__)
 def home():
     return "API funcionando"
 
-@app.route("/barcode")
-def gerar_barcode():
-    texto = request.args.get("text")
-    if not texto:
-        abort(400, "Parâmetro 'text' é obrigatório")
-
-    code = barcode.get("code128", texto, writer=ImageWriter())
-
-    buffer = io.BytesIO()
-    code.write(buffer)
-    buffer.seek(0)
-
-    return send_file(buffer, mimetype="image/png")
